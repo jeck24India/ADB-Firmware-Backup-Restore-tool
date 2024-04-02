92b85b0c2f71c47e3e880ba0aa8b9c3da8484489
jeck24India <110726589+jeck24India@users.noreply.github.com>
2024-04-02 11:19:39 +0100

    Add files via upload

---
+++ Partition-Manager/Program.cs
@@ -0,0 +1,25 @@
+﻿using Partition_Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Classification.BoW
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
